f7757700c91a7945b66302792ce361b2e459f040
ids09 <idsart.dikkers@student.hu.nl>
2018-09-21 18:43:55 +0200

    deel 1

---
+++ week 2/opdrachten in de les 13-9-2018.py
@@ -0,0 +1,26 @@
+#A
+a= 10
+b= 10.4
+c= 'hello'
+d= False
+
+#B
+totaal= (a+b)
+
+#C
+print(c*(int(totaal)))
+
+
+
+import math
+a=math.sqrt(3**2+4**2)
+print (a)
+
+print(a==5)
+c=math.pi*10**2
+print (c)
+
+d=2**5+2**5
+print (d)
+e=d<7**2
+print
